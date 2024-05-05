c93bc5ab1e133a213f0bd06152a09f022a5d70b5
22730033 Võ Minh Hiếu <127770575+hieuvouit@users.noreply.github.com>
2024-05-05 21:59:13 +0700

    Tinh nang: Ve Object con ran

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("Running....");
+var canvas = document.getElementById('game');
+var context = canvas.getContext('2d');
+var grid = 20;
+var snake = {
+    x: 160,
+    y: 160,
+    dx: grid,
+    dy: 0,
+    cells: [],
+    maxCells: 4,
+};
+
+// game loop
+function loop() {
+    requestAnimationFrame(loop);
+    // thêm vị trí mà con rắn đi qua, index số 0 là đầu rắn
+    snake.cells.unshift({
+        x: snake.x,
+        y: snake.y,
+    });
+
+    // Duyệt thân và vẽ rắn
+    context.fillStyle = 'green';
+    snake.cells.forEach(function (cell, index) {
+        context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
+    });
+}
+// hàm gọi game
+requestAnimationFrame(loop);
