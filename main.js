094b07f990f61eb1337a4ddd8e4ebe1ff6b2ef3c
22730026.VoDucDung <127115219+22730026-VoDucDung@users.noreply.github.com>
2024-05-08 23:24:45 +0700

    Thêm chức năng hiển thị bảng xếp hạng trò chơi (#5)

--- main.js
+++ main.js
@@ -29,8 +29,32 @@ window.onload = function () {
     snake.cells.forEach(function (cell, index) {
       context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
     });
-  }
+    
+    // Bảng xếp hạng
+var leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
 
-  // hàm gọi game
-  requestAnimationFrame(loop);
-};
+function saveLeaderboard() {
+    localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
+}
+
+function updateLeaderboard() {
+    leaderboard.push(score);
+    leaderboard.sort((a, b) => b - a);
+    leaderboard = leaderboard.slice(0, 5);
+    saveLeaderboard();
+    displayLeaderboard();
+}
+
+function displayLeaderboard() {
+    var leaderboardElement = document.getElementById('leaderboard');
+    leaderboardElement.innerHTML = 'Leaderboard:<br>';
+    leaderboard.forEach((score, index) => {
+        leaderboardElement.innerHTML += `${index + 1}. ${score}<br>`;
+    });
+}
+    
+// hàm gọi game
+requestAnimationFrame(loop);
+displayLeaderboard();    
+}
+
