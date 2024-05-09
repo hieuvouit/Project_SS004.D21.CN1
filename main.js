b9e0095891695b1de4721e1180e405a22c6c2585
NhutNguyen16 <127914103+NhutNguyen16@users.noreply.github.com>
2024-05-09 10:28:02 +0700

    Thêm phần điều khiển rắn

--- main.js
+++ main.js
@@ -51,6 +51,30 @@ window.onload = function () {
           leaderboardElement.innerHTML += `${index + 1}. ${score}<br>`;
       });
   }
+
+  // Hàm điều khiển rắn
+  document.addEventListener('keydown', function(e) {
+    // trái
+    if (e.which === 37 && snake.dx === 0) {
+        snake.dx = -grid;
+        snake.dy = 0;
+    }
+    // lên
+    else if (e.which === 38 && snake.dy === 0) {
+        snake.dy = -grid;
+        snake.dx = 0;
+    }
+    // phải
+    else if (e.which === 39 && snake.dx === 0) {
+        snake.dx = grid;
+        snake.dy = 0;
+    }
+    // xuống
+    else if (e.which === 40 && snake.dy === 0) {
+        snake.dy = grid;
+        snake.dx = 0;
+    }
+  });
     
 // hàm gọi game
   requestAnimationFrame(loop);
